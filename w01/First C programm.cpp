3bd6bc400febe9dbb83b3ee6a492a13c78c4dc36
j0hnng <johnn9sb@gmail.com>
2018-09-17 17:27:13 +0800

    Adding w01 files

---
+++ w01/First C programm.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	
+  printf("Welcome to C Programming\n");
+  printf("My name: htc\n");	
+  return 0;
+}
+
---
+++ w01/Temperature convert C.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	float C, F;
+	printf("Enter temperature in C: ");
+	scanf("%f",&C);
+	F=C*9/5+32;	
+	printf("%.1f C = %.1f F",C,F);
+}
+
+/*
+Enter temperature in C: 20.5
+20.5 C =68.9 F
+Enter temperature in F: 80.5
+80.5 F =26.9 C 
+*/
